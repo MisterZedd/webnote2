0f837f139e35edea1509bcaa9fa839161dcf568a
MisterZedd <93245713+MisterZedd@users.noreply.github.com>
2025-03-24 11:44:25 -0400

    Add files via upload

---
+++ save.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+from etc.common import *
+from lib import *
+
+# the main method is here
+try:
+    ws = Workspace.CreateSave()
+    updateTime = ws.commit()
+    ws.trimSaveHistory(10)
+    if updateTime:
+        print 'Content-type: text/xml\n'
+        print ('<return>\n'
+               '  <status value="ok" update="%s"/>\n'
+               '</return>'
+               % str(updateTime))
+    else:
+        retMsg('error')
+except Exception, e:
+  message.Error(e)
